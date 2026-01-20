70e87f7c84d762c1f3978657785c8b7425f67ce7
plamenc80 <chatalbashevp@gmail.com>
2026-01-20 15:40:22 +0200

    .

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PermuntaciqVariaciqKombinaciqZadacha7
+{
+    internal class Program
+    {
+        static long Fact(int x)
+        {
+            long f = 1;
+            for (int i = 1; i <= x; i++)
+                f *= i;
+            return f;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedete chislo n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Vuvedete chislo k: ");
+            int k = int.Parse(Console.ReadLine());
+            long permutations = Fact(n) / Fact(n - k);
+            long variations = Fact(n) / Fact(n - k);
+            long combinations = Fact(n) / (Fact(k) * Fact(n - k));
+            Console.WriteLine($"Permutacii: {permutations}");
+            Console.WriteLine($"Variacii: {variations}");
+            Console.WriteLine($"Kombinacii: {combinations}");
+                
+        }
+    }
+}
